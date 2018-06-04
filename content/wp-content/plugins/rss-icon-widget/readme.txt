75583ed3bb02dc17c17dae6c7a96a82de08dcf16
Tom Mason <wheybags@wheybags.com>
2018-06-04 21:12:11 +0100

    update wp

--- content/wp-content/plugins/rss-icon-widget/readme.txt
+++ content/wp-content/plugins/rss-icon-widget/readme.txt
@@ -3,12 +3,12 @@ Contributors: brandondove, jeffreyzinn
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=3497105
 Tags: RSS Feed, Feed Icon, Widget, Sidebar
 Requires at least: 2.8
-Tested up to: 3.5.1
-Stable tag: 4.0
+Tested up to: 4.7.4
+Stable tag: 5.1
 
 == Description ==
 
-We had a client who want to be able to put a link to his rss feed in his sidebar. Visit studiolightroom.com to check it out. Oddly enough we didn’t find anything readily available to do that without adding code to his theme files. We decided to create a little widget to handle this functionality. It has the following features and is freely available right here:
+We had a client who want to be able to put a link to his rss feed in his sidebar. Oddly enough we didn’t find anything readily available to do that without adding code to his theme files. We decided to create a little widget to handle this functionality. It has the following features and is freely available right here:
 
 1. Customizable link text
 2. Customizable link color
@@ -16,6 +16,8 @@ We had a client who want to be able to put a link to his rss feed in his sidebar
 4. Enter any feed URL
 
 http://www.youtube.com/watch?v=piDZibMb8NU
+
+We've added a new widget that will dynamically construct the Feed URL for the archive that the user is looking at. This means if a user is looking at the blog home page, the feed will be the main blog feed. However if the user is looking at a specific category, the URL for the feed will be of that specific category. If the user is not visiting an archive page, the widget will not be displayed.
 
 If you have any problems with this widget, please don't hesitate to let us know. If you use the widget and like it, please rate it.
 
@@ -53,3 +55,9 @@ Ok. If you don't see mention of widgets in the WordPress admin, you're not crazy
 
 = 4.0 =
 - Added option to open links in a new window (props B.Chamberlain)
+
+= 5.0 =
+- Changed widget constructors
+
+= 5.1 =
+- Added new Dynamic RSS Icon Widget that dynamically constructs the URL of the feed for the archive that the user is looking at
--- content/wp-content/plugins/rss-icon-widget/rss-icon-widget.php
+++ content/wp-content/plugins/rss-icon-widget/rss-icon-widget.php
@@ -1,36 +1,32 @@
 <?php
 /*
 Plugin Name: RSS Icon Widget
-Plugin URI: http://pixeljar.net/widgets/rss-icon-widget
-Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=3497105
 Description: The idea is to have a widget to display a link to any rss feed with a <a href="http://www.feedicons.com">standard feed icon</a>.
+Plugin URI: http://pixeljar.com
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=3497105
 Author: Pixel Jar
-Version: 4.0
-Author URI: http://pixeljar.net
+Author URI: http://www.pixeljar.com
 Contributors: brandondove, jeffreyzinn
-*/
+Version: 5.1
+License: GPL2
+Text Domain: rssiw
+Domain Path: /i18n
 
-/**
- * Copyright (c) 2012 Pixel Jar. All rights reserved.
- *
- * Released under the GPL license
- * http://www.opensource.org/licenses/gpl-license.php
- *
- * This is an add-on for WordPress
- * http://wordpress.org/
- *
- * **********************************************************************
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * **********************************************************************
- */
+    Copyright (C) Jul 24, 2017  Pixel Jar  info@pixeljar.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 
 define( 'RSSIW_URL',	plugin_dir_url( __FILE__ ) );
 define( 'RSSIW_ABS',	plugin_dir_path( __FILE__ ) );
@@ -53,8 +49,8 @@ class RSSIconWidget extends WP_Widget {
 	);
 
     /** constructor */
-    function RSSIconWidget() {
-		parent::WP_Widget(
+    function __construct() {
+		parent::__construct(
 			false,
 			$name = __( 'RSS Icon Widget', 'rssiw' ),
 			$widget_options = array(
@@ -71,7 +67,7 @@ class RSSIconWidget extends WP_Widget {
 		// URL
 		echo '<a href="'.esc_url( $instance['feed_url'] ).'" ';
 
-			echo ( $instance['new_window'] == 1 ? 'target="_blank" ' : '' );
+			echo ( isset( $instance['new_window'] ) && $instance['new_window'] == 1 ? 'target="_blank" ' : '' );
 
 			// Styles
 			echo 'style="'.
@@ -157,3 +153,113 @@ function rssiconwidget_admin_scripts() {
 	wp_enqueue_script( 'farbtastic' );
 	wp_enqueue_style( 'farbtastic' );
 }
+
+class DynamicRSSIconWidget extends WP_Widget {
+
+	var $icon_sizes = array(
+		'10' => '10 x 10',
+		'12' => '12 x 12',
+		'14' => '14 x 14',
+		'16' => '16 x 16',
+		'24' => '24 x 24',
+		'32' => '32 x 32'
+	);
+
+    /** constructor */
+    function __construct() {
+		parent::__construct(
+			false,
+			$name = __( 'Dynamic RSS Icon Widget', 'rssiw' ),
+			$widget_options = array(
+				'description' => __( 'Display a link with the standard RSS Feed Icon linked to the RSS feed for the archive page you\'re viewing. Note: This widget will only display on archive pages like the main blog list.', 'rssiw' )
+			)
+		);
+    }
+
+    /** @see WP_Widget::widget */
+    function widget( $args, $instance) {
+
+    	if ( ! is_archive() && ! is_home() ) {
+    		return;
+    	}
+
+        extract( $args );
+
+		echo $before_widget;
+		// URL
+		$url = trailingslashit( home_url( $_SERVER['REQUEST_URI'] ) ) . 'feed/';
+
+		echo '<a href="'.esc_url( $url ).'" ';
+
+			echo ( isset( $instance['new_window'] ) && $instance['new_window'] == 1 ? 'target="_blank" ' : '' );
+
+			// Styles
+			echo 'style="'.
+
+				// Color
+				'color: '.$instance['link_color'].'; '.
+
+				// Padding
+				'padding: '.( $instance['image_size'] / 2 ).'px 0px '.
+								 ( $instance['image_size'] / 2 ).'px '.
+								 ( $instance['image_size'] + 5 ).'px; '.
+				// Image
+				'background: url(\''.RSSIW_URL.'icons/feed-icon-'.$instance['image_size'].'x'.$instance['image_size'].'.png\') no-repeat 0 50%;">';
+			echo esc_html( $instance['link_text'] );
+		echo '</a>';
+		echo $after_widget;
+    }
+
+    /** @see WP_Widget::update */
+    function update( $new_instance, $old_instance ) {
+		$updated_instance = $new_instance;
+		return $updated_instance;
+    }
+
+    /** @see WP_Widget::form */
+    function form( $instance = array() ) {
+		$defaults = array(
+			'image_size'	=> '10',
+			'link_text'		=> __( 'Subscribe via RSS', 'rssiw' ),
+			'link_color'	=> '#ff0000',
+			'new_window'	=> '0',
+		);
+		extract( wp_parse_args( $instance, $defaults ) );
+		?>
+			<p><label for="<?php echo $this->get_field_id( 'image_size' ); ?>"><?php _e( 'Icon Size:', 'rssiw' ) ?><br />
+				<select id="<?php echo $this->get_field_id( 'image_size' ); ?>" name="<?php echo $this->get_field_name( 'image_size' ); ?>" style="width: 100%;">
+					<?php
+						foreach( $this->icon_sizes as $size_key => $size_name) :
+							echo '  <option value="'.$size_key.'"'.selected( $image_size, $size_key, true ).'>'.$size_name.'</option>';
+						endforeach;
+					?>
+				</select>
+			</label></p>
+
+			<p><label for="<?php echo $this->get_field_id( 'link_text' ); ?>"><?php _e( 'Link Text:', 'rssiw' ) ?><br />
+				<input id="<?php echo $this->get_field_id( 'link_text' ); ?>" name="<?php echo $this->get_field_name( 'link_text' ); ?>" type="text" style="width: 100%;" value="<?php echo esc_attr( $link_text ) ?>" />
+			</label></p>
+
+			<script type="text/javascript">
+				jQuery(document).ready(function($) {
+					$('#<?php
+						echo $this->get_field_id( 'link_color' );
+					?>_colorpicker').farbtastic('#<?php
+						echo $this->get_field_id( 'link_color' );
+					?>');
+				});
+			</script>
+			<p><label for="rssicon_link_color"><?php _e( 'Link Color:', 'rssiw' ); ?><br />
+				<div id="<?php echo $this->get_field_id( 'link_color' ); ?>_colorpicker"></div>
+				<input id="<?php echo $this->get_field_id( 'link_color' ); ?>" name="<?php echo $this->get_field_name( 'link_color' ); ?>" type="text" style="width: 100%;" value="<?php echo esc_attr( $link_color ); ?>" />
+			</label></p>
+
+			<p><input id="<?php echo $this->get_field_id( 'new_window' ); ?>" name="<?php echo $this->get_field_name( 'new_window' ); ?>" type="checkbox" value="1" <?php checked( 1, $new_window ) ?> />
+			<label for="<?php echo $this->get_field_id( 'new_window' ); ?>"><?php _e( 'Open in a new window?', 'rssiw' ) ?></label></p>
+		<?php
+    }
+
+} // class DynamicRSSIconWidget
+
+// register DynamicRSSIconWidget widget
+add_action( 'widgets_init', create_function( '', 'return register_widget("DynamicRSSIconWidget");' ) );
